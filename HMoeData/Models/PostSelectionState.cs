4bbde65ad211bd7a77f9ddb344f9491fbae39287
Poker <poker_sang@outlook.com>
2026-08-10 16:40:07 +0800

    注释

--- HMoeData/Models/PostSelectionState.cs
+++ HMoeData/Models/PostSelectionState.cs
@@ -2,8 +2,23 @@ namespace HMoeData.Models;
 
 public enum PostSelectionState
 {
+    /// <summary>
+    /// 未选
+    /// </summary>
     Unselected,
+    
+    /// <summary>
+    /// 已选
+    /// </summary>
     Selected,
+
+    /// <summary>
+    /// 取消选择（查看详情后取消）
+    /// </summary>
     Deselected,
+
+    /// <summary>
+    /// 已删除（下载后删除）
+    /// </summary>
     Deleted
 }
